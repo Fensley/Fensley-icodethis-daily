f637b42e1e8de27d03324bcd0e659fe75646dd3f
Fensley <fortilusnehemie@gmail.com>
2024-09-04 00:10:24 -0400

    initial commit

---
+++ Countdown/scripts.js
@@ -0,0 +1,14 @@
+const month = document.querySelector(".months");
+
+const days = document.querySelector(".days");
+
+const hours = document.querySelector(".hours");
+
+const minutes = document.querySelector(".minutes");
+
+const second = document.querySelector(".seconds");
+
+const date = new Date();
+
+console.log(date);
+// be back
