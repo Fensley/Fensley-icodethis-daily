8dc8dc9c423caa1057b59826f9df61e090f9b1e6
Fensley <fortilusnehemie@gmail.com>
2024-09-05 13:48:50 -0400

    initial commit

---
+++ review-1/data.js
@@ -0,0 +1,26 @@
+const data = {
+  expImgOne:
+    "https://images.unsplash.com/photo-1533636721434-0e2d61030955?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  expImgTwo:
+    "https://images.unsplash.com/photo-1579634151863-8ddedd098251?q=80&w=2811&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  expNameOne: "Fess kiins",
+  expNameTwo: "Mees pacedi",
+  expParaOne:
+    "Suspendisse bibendum sed libero eu ultrices. Morbi condimentum tempor ullamcorper.",
+  expParaTwo:
+    "Nulla non nibh non augue scelerisque malesuada. Maecenas eleifend purus ut tellus ultrices aliquet.",
+
+  quaNameOne: "hendrerit nunc",
+  quaNameTwo: "Aliquam metus",
+  quaParaOne:
+    "Nulla ac lectus luctus, porttitor orci quis, mollis orci. Nullam rutrum tempus iaculis.",
+  quaParaTwo:
+    "Maecenas volutpat lacinia ante, at convallis lacus condimentum quis. Integer vitae nibh nec ",
+
+  quaImgOne:
+    "https://images.unsplash.com/photo-1703850303972-a1b9e8d8c07f?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  quaImgTwo:
+    "https://images.unsplash.com/photo-1563245110-4ba1be9ae814?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+export default data;
