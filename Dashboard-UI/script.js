c410a1a2e3a1d8bcd7fbf68fa1d5a5c22dee78ab
Fensley <fortilusnehemie@gmail.com>
2024-09-08 06:51:27 -0400

    initial commit

--- Dashboard-UI/script.js
+++ Dashboard-UI/script.js
@@ -1,24 +1,51 @@
-(async function () {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
-
-  new Chart(document.getElementById("acquisitions"), {
-    type: "bar",
-    data: {
-      labels: data.map((row) => row.year),
-      datasets: [
-        {
-          label: "Acquisitions by year",
-          data: data.map((row) => row.count),
-        },
-      ],
-    },
-  });
-})();
+const menu = document.querySelector(".fa-bars");
+const Dashboard = document.querySelector(".das");
+const project = document.querySelector(".pro");
+const user = document.querySelector(".use");
+const profileData = document.querySelector(".too-data");
+const hiddenpara = document.querySelector(".hiddendata");
+const direct = document.querySelector(".direct");
+const social = document.querySelector(".social");
+const organic = document.querySelector(".organic");
+// console.log(direct);
+menu.addEventListener("click", () => {
+  console.log("here");
+  profileData.style.display = "none";
+  Dashboard.style.display = "block";
+  project.style.display = "block";
+  user.style.display = "block";
+});
+Dashboard.addEventListener("click", () => {
+  profileData.style.display = "flex";
+  Dashboard.style.display = "none";
+  project.style.display = "none";
+  user.style.display = "none";
+  hiddenpara.style.visibility = "visible";
+  hiddenpara.innerHTML = "Dashboard";
+  direct.innerHTML = "24% Direct";
+  social.innerHTML = "31% Social";
+  organic.innerHTML = "45% Organic";
+});
+project.addEventListener("click", () => {
+  profileData.style.display = "flex";
+  Dashboard.style.display = "none";
+  project.style.display = "none";
+  user.style.display = "none";
+  hiddenpara.style.visibility = "visible";
+  hiddenpara.innerHTML = "Project";
+  direct.innerHTML = "10% Direct";
+  social.innerHTML = "50% Social";
+  organic.innerHTML = "15% Organic";
+});
+user.addEventListener("click", () => {
+  profileData.style.display = "flex";
+  Dashboard.style.display = "none";
+  project.style.display = "none";
+  user.style.display = "none";
+  hiddenpara.style.visibility = "visible";
+  hiddenpara.innerHTML = "User";
+  direct.innerHTML = "71% Direct";
+  social.innerHTML = "09% Social";
+  organic.innerHTML = "10% Organic";
+});
+console.log();
