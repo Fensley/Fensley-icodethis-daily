63b41c9dbb3e077c9a9979406a88fea26dcf5772
Fensley <fortilusnehemie@gmail.com>
2024-10-24 12:58:17 -0400

    initial commit

---
+++ pricing-table/script.js
@@ -0,0 +1,17 @@
+const type = document.querySelectorAll(".active-y");
+const monthly = document.querySelector(".monthly");
+const yearly = document.querySelector(".yearly");
+
+monthly.addEventListener("click", () => {
+  if (yearly.classList.contains("testing")) {
+    yearly.classList.remove("testing");
+    monthly.classList.add("testing");
+  }
+});
+
+yearly.addEventListener("click", () => {
+  if (monthly.classList.contains("testing")) {
+    yearly.classList.add("testing");
+    monthly.classList.remove("testing");
+  }
+});
