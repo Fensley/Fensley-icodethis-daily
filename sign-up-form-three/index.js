40533f53258baad52c229619eb835ab69f44d027
Fensley <fortilusnehemie@gmail.com>
2024-10-26 15:36:54 -0400

    intial commit

--- sign-up-form-three/index.js
+++ sign-up-form-three/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import ejs from "ejs";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const userName = req.body["Fname"];
+  res.render("receive.ejs", { userName });
+  // console.log(req.body);
 });
 
 app.listen(port, (err) => {
